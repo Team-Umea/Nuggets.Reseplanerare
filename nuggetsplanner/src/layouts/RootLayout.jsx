5a209e24434f8ab5142502ac4d109ac764dbb24f
Burtarn <robin.andersson0125@gmail.com>
2025-02-05 13:55:38 +0100

    Group-effort on building homepage.

--- nuggetsplanner/src/layouts/RootLayout.jsx
+++ nuggetsplanner/src/layouts/RootLayout.jsx
@@ -1,16 +1,19 @@
 import { Outlet, NavLink } from "react-router-dom"
+import '../styles/RootLayout.css'
 
 function RootLayout() {
     return ( <div>
         <header>
         <h1>Reseplaneraren</h1>
         <nav>
+            
             <ul>
                 <NavLink to="/">Hem</NavLink>
                 <NavLink to="about">Om</NavLink>
                 <NavLink to="tripdetails">Detaljer</NavLink>
             </ul>
         </nav>
+
         </header>
         <main>
         <Outlet />
--- nuggetsplanner/src/pages/Home.jsx
+++ nuggetsplanner/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { fetchInformationApi } from "../api/Api";
+import '../styles/Home.css'
 
 function HomePage() {
   const [information, setInformation] = useState([]);
@@ -14,12 +15,18 @@ function HomePage() {
 
   return (
     <div>
-      <h1>Hem</h1>
+      <h2 className="webTitle">Välkommen till Reseplaneraren</h2>
+      <p className="titleText">Detta är en sida som kan hjälpa dig att organisera dina framtida drömmar och resor. Samt hitta insperation till potentiella drömresor!</p>
+
       <ul>
         {information.map((location, index) => (
-          <li key={index}>
-            {location.activity},{location.location},{location.image}
-          </li>
+          <div className="infoContainer" key={index}>
+            <img src={location.url} alt="Bild på resmål" className="infoImg"/>
+            <h3 className="infoLocation">{location.location}</h3>
+            <p className="infoActivity">{location.activity}</p>
+
+
+          </div>
         ))}
       </ul>
     </div>
@@ -27,3 +34,4 @@ function HomePage() {
 }
 
 export default HomePage;
+
