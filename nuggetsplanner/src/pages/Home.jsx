54e08ca668696b68f93690772c4946c8d94876ad
Frank <Frankuik@hotmail.com>
2025-02-05 12:44:48 +0100

    FetchfunctinoN on home done. commit Tobias,Robin,Frank

--- nuggetsplanner/src/pages/Home.jsx
+++ nuggetsplanner/src/pages/Home.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+import { fetchInformationApi } from "../api/Api";
+
 function HomePage() {
-    return ( 
-        <h1>HEM!</h1>
-     );
+  const [information, setInformation] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchInformationApi();
+      setInformation(data);
+      console.log(data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Hem</h1>
+      <ul>
+        {information.map((location, index) => (
+          <li key={index}>
+            {location.activity},{location.location},{location.image}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default HomePage;
