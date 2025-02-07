86fe8df6f6ebb59e3141f369237f9767694479ce
Burtarn <robin.andersson0125@gmail.com>
2025-02-07 15:36:35 +0100

    FAQ - done.

--- nuggetsplanner/src/App.jsx
+++ nuggetsplanner/src/App.jsx
@@ -2,6 +2,8 @@
 import { RouterProvider } from "react-router-dom"
 import './styles/App.css'
 import router from "./Router.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return <RouterProvider router={router} />;
