dde94b7860519abc884b0119e9206822d49ee375
EliaAssaf19 <elias.assaf@chasacademy.se>
2025-02-09 20:37:56 +0100

    Contact form done -Elias

--- nuggetsplanner/src/pages/help/Contact.jsx
+++ nuggetsplanner/src/pages/help/Contact.jsx
@@ -1,6 +1,72 @@
+import React, { useState } from 'react';
+import '../../styles/Contact.css';
+
 function Contact() {
-    return ( 
-        <h2>Contact</h2>
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('formulär-data skickad: ', formData);
+        alert('Tack för ditt meddelande!');
+        setFormData({ name: '', email: '', message: '' });
+    };
+
+    return (
+        <div className="contact-container">
+           <h2>Kontakta Oss</h2>
+           <form className='contact-form' onSubmit={handleSubmit}>
+            <div className='form-group'>
+                <label htmlFor="name">Name</label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    placeholder="Ange ditt namn"
+                    required
+                />
+            </div>
+            <div className='form-group'>
+                <label htmlFor="email">Email</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    placeholder="Ange din Email"
+                    required
+                />
+            </div>
+            <div className='form-group'>
+                <label htmlFor="message">Meddelande</label>
+                <textarea
+                    id="message"
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                    placeholder="Ange ditt meddelande"
+                    rows="5"
+                    required
+                ></textarea>
+            </div>
+            <button type="submit" className="submit-button">Skicka Meddelande</button>
+            </form> 
+        </div> 
+        
      );
 }
 
