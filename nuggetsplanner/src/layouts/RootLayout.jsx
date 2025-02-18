9cda2dda1f3debc98cbe6572d14f52f71d170ed2
Frank <Frankuik@hotmail.com>
2025-02-18 11:10:15 +0100

    sdfa

--- nuggetsplanner/src/layouts/RootLayout.jsx
+++ nuggetsplanner/src/layouts/RootLayout.jsx
@@ -1,25 +1,11 @@
 import { Outlet, NavLink } from "react-router-dom";
+import Header from "../components/header/header";
 import "../styles/RootLayout.css";
 
 function RootLayout() {
   return (
     <div>
-      <header>
-        <h1>Reseplaneraren</h1>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Hem</NavLink>
-            </li>
-            <li>
-              <NavLink to="/about" className={({ isActive }) => (isActive ? "active" : "")}>Om</NavLink>
-            </li>
-            <li>
-              <NavLink to="/tripdetails" className={({ isActive }) => (isActive ? "active" : "")}>Detaljer</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <Header></Header>
 
       <main>
         <Outlet />
